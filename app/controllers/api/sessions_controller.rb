f13f7fedf71cb1a9d75ee4161ecbb79daf53920d
ryan-mapa <ryan.m.mapa@gmail.com>
2018-10-14 11:47:12 -0400

    Auth controller updates

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,23 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = user.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(user)
+      render json: @user
+    else
+      render json: ['ya messed up'], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render 'api/users/show'
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
+  end
+
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,24 +1,28 @@
 class Api::UsersController < ApplicationController
-  def new
-  end
-
-  def edit
-  end
-
-  def update
-  end
 
   def create
+    @user = User.new(user_params)
+      if @user.save
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: @users.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+
   end
 
   def show
     @user = User.find(params[:id])
-    
+    render 'api/users/show'
   end
 
   def index
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
 end
